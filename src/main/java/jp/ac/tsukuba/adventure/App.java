4f3bb448947bc5d8262a97a6424c1890ea06b5d0
Axarva <timsina@axarva.me>
2026-02-05 18:28:13 +0900

    Initial project setup

---
+++ src/main/java/jp/ac/tsukuba/adventure/App.java
@@ -0,0 +1,13 @@
+package jp.ac.tsukuba.adventure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
